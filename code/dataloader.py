1006dfb2aea9edf0b3f039301bae6bcf8a9827b2
frameartist <seanxionghk@gmail.com>
2023-04-08 16:40:14 +0800

    add movielens dataset

--- code/dataloader.py
+++ code/dataloader.py
@@ -19,6 +19,8 @@ import scipy.sparse as sp
 import world
 from world import cprint
 from time import time
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
 
 class BasicDataset(Dataset):
     def __init__(self):
@@ -374,7 +376,7 @@ class Loader(BasicDataset):
             dict: {user: [items]}
         """
         test_data = {}
-        for i, item in enumerate(self.testItem):
+        for i, item in enumerate(self.testItem[:100]):
             user = self.testUser[i]
             if test_data.get(user):
                 test_data[user].append(item)
@@ -405,3 +407,188 @@ class Loader(BasicDataset):
     #     for user in users:
     #         negItems.append(self.allNeg[user])
     #     return negItems
+
+class MovieLens(BasicDataset):
+    def __init__(self,config = world.config,path="../data/ml_latest_small/"):
+        # train or test
+        cprint(f'loading [{path}]')
+        self.split = config['A_split']
+        self.folds = config['A_n_fold']
+        self.mode_dict = {'train': 0, "test": 1}
+        self.mode = self.mode_dict['train']
+        
+        # Load train dataset
+        dataset = pd.read_csv(path+"ratings.csv", names=["userId","movieId","rating","timestamp"])
+        
+        le = LabelEncoder()
+        dataset["userId"] = le.fit_transform(dataset["userId"])
+        dataset["movieId"] = le.fit_transform(dataset["movieId"])
+
+        dataset, test_dataset = train_test_split(dataset, test_size=0.1)
+
+        dataset.to_csv("ml_train.csv")
+        test_dataset.to_csv("ml_test.csv")
+
+        users = dataset["userId"]
+        items = dataset["movieId"]
+        ratings = dataset["rating"]
+        #+++++++++++++++++++
+
+        # Load test dataset
+        # test_dataset = pd.read_csv(path+"ratings.csv", names=["userId","movieId","rating","timestamp"])
+        # Loaded already^
+
+        test_users = test_dataset["userId"]
+        test_items = test_dataset["movieId"]
+        test_ratings = test_dataset["rating"]
+
+        self.n_user = users.max() + 1
+        self.m_item = items.max() + 1
+        self.path = path
+
+        trainUniqueUsers, trainItem, trainUser, trainRating = users.drop_duplicates(), items, users, ratings
+        testUniqueUsers, testItem, testUser, testRating = test_users.drop_duplicates(), test_items, test_users, test_ratings
+        
+        self.traindataSize = len(dataset["userId"])
+        self.testDataSize = len(test_dataset["userId"])
+
+        self.trainUniqueUsers = np.array(trainUniqueUsers)
+        self.trainUser = np.array(trainUser)
+        self.trainItem = np.array(trainItem)
+        self.trainRating = np.array(trainRating)
+
+        self.testUniqueUsers = np.array(testUniqueUsers)
+        self.testUser = np.array(testUser)
+        self.testItem = np.array(testItem)
+        self.testRating = np.array(testRating)
+        
+        self.Graph = None
+        print(f"{self.trainDataSize} interactions for training")
+        print(f"{self.testDataSize} interactions for testing")
+        print(f"{world.dataset} Sparsity : {(self.trainDataSize + self.testDataSize) / self.n_users / self.m_items}")
+
+        # (users,items), bipartite graph
+        self.UserItemNet = csr_matrix((np.ones(len(self.trainUser)), (self.trainUser, self.trainItem)),
+                                      shape=(self.n_user, self.m_item))
+        self.users_D = np.array(self.UserItemNet.sum(axis=1)).squeeze()
+        self.users_D[self.users_D == 0.] = 1
+        self.items_D = np.array(self.UserItemNet.sum(axis=0)).squeeze()
+        self.items_D[self.items_D == 0.] = 1.
+        # pre-calculate
+        self._allPos = self.getUserPosItems(list(range(self.n_user)))
+        self.__testDict = self.__build_test()
+        print(f"{world.dataset} is ready to go, ml")
+    
+    @property
+    def n_users(self):
+        return self.n_user
+    
+    @property
+    def m_items(self):
+        return self.m_item
+    
+    @property
+    def trainDataSize(self):
+        return self.traindataSize
+    
+    @property
+    def testDict(self):
+        return self.__testDict
+
+    @property
+    def allPos(self):
+        return self._allPos
+
+    def _split_A_hat(self,A):
+        A_fold = []
+        fold_len = (self.n_users + self.m_items) // self.folds
+        for i_fold in range(self.folds):
+            start = i_fold*fold_len
+            if i_fold == self.folds - 1:
+                end = self.n_users + self.m_items
+            else:
+                end = (i_fold + 1) * fold_len
+            A_fold.append(self._convert_sp_mat_to_sp_tensor(A[start:end]).coalesce().to(world.device))
+        return A_fold
+
+    def _convert_sp_mat_to_sp_tensor(self, X):
+        coo = X.tocoo().astype(np.float32)
+        row = torch.Tensor(coo.row).long()
+        col = torch.Tensor(coo.col).long()
+        index = torch.stack([row, col])
+        data = torch.FloatTensor(coo.data)
+        return torch.sparse.FloatTensor(index, data, torch.Size(coo.shape))
+        
+    def getSparseGraph(self):
+        print("loading adjacency matrix")
+        if self.Graph is None:
+            try:
+                pre_adj_mat = sp.load_npz(self.path + '/s_pre_adj_mat.npz')
+                print("successfully loaded...")
+                norm_adj = pre_adj_mat
+            except :
+                print("generating adjacency matrix")
+                s = time()
+                adj_mat = sp.dok_matrix((self.n_users + self.m_items, self.n_users + self.m_items), dtype=np.float32)
+                adj_mat = adj_mat.tolil()
+                R = self.UserItemNet.tolil()
+                adj_mat[:self.n_users, self.n_users:] = R
+                adj_mat[self.n_users:, :self.n_users] = R.T
+                adj_mat = adj_mat.todok()
+                # adj_mat = adj_mat + sp.eye(adj_mat.shape[0])
+                
+                rowsum = np.array(adj_mat.sum(axis=1))
+                d_inv = np.power(rowsum, -0.5).flatten()
+                d_inv[np.isinf(d_inv)] = 0.
+                d_mat = sp.diags(d_inv)
+                
+                norm_adj = d_mat.dot(adj_mat)
+                norm_adj = norm_adj.dot(d_mat)
+                norm_adj = norm_adj.tocsr()
+                end = time()
+                print(f"costing {end-s}s, saved norm_mat...")
+                sp.save_npz(self.path + '/s_pre_adj_mat.npz', norm_adj)
+
+            if self.split == True:
+                self.Graph = self._split_A_hat(norm_adj)
+                print("done split matrix")
+            else:
+                self.Graph = self._convert_sp_mat_to_sp_tensor(norm_adj)
+                self.Graph = self.Graph.coalesce().to(world.device)
+                print("don't split the matrix")
+        return self.Graph
+    
+
+    def __build_test(self):
+        """
+        return:
+            dict: {user: [items]}
+        """
+        test_data = {}
+        for i, item in enumerate(self.testItem):
+            user = self.testUser[i]
+            if test_data.get(user):
+                test_data[user].append(item)
+            else:
+                test_data[user] = [item]
+        return test_data
+    
+    def getUserItemFeedback(self, users, items):
+        """
+        users:
+            shape [-1]
+        items:
+            shape [-1]
+        return:
+            feedback [-1]
+        """
+        # print(self.UserItemNet[users, items])
+        return np.array(self.UserItemNet[users, items]).astype('uint8').reshape((-1,))
+
+    def getUserPosItems(self, users):
+        posItems = []
+        for user in users:
+            posItems.append(self.UserItemNet[user].nonzero()[1])
+        return posItems
+
+    
